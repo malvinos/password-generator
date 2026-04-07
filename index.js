82c741007711c2f54f56c61332142874f7b2d3cf
malvinos <75187206+malvinos@users.noreply.github.com>
2026-04-07 15:42:39 +0200

    Initial commit

---
+++ index.js
@@ -0,0 +1,21 @@
+const characters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9","~","`","!","@","#","$","%","^","&","*","(",")","_","-","+","=","{","[","}","]",",","|",":",";","<",">",".","?",
+"/"];
+
+let generateBtnEl = document.getElementById("generate-btn")
+let passwordOneEl = document.getElementById("password-one")
+let passwordTwoEl = document.getElementById("password-two")
+
+function generatePasswords() {
+	let passwordone = ""
+	let passwordtwo = ""
+	for (let i = 0; i < 15; i++) {
+		passwordone += characters[randomIndex()]
+		passwordtwo += characters[randomIndex()]
+	}
+	passwordOneEl.textContent = passwordone
+	passwordTwoEl.textContent = passwordtwo
+}
+
+function randomIndex() {
+	return Math.floor(Math.random()*characters.length)
+}
